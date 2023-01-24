fec96c1f87d231475a0f7d00bf213bcc4b6d293c
MrRobot-29 <gaurav.dash@flipkart.com>
2023-01-24 14:25:16 +0530

    Adding STS project

---
+++ src/com/flipkart/app/DemoClass.java
@@ -0,0 +1,11 @@
+package com.flipkart.app;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java from STS");
+
+	}
+
+}
